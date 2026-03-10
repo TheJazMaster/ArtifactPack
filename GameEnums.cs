e5feeb192057937b30e922e4cd6e126e01d9fefb
Aljaž Perovšek <aljazpero@hotmail.com>
2026-03-10 17:20:41 +0100

    Initial commit

---
+++ GameEnums.cs
@@ -0,0 +1,9 @@
+﻿using Nanoray.EnumByNameSourceGenerator;
+
+namespace TheJazMaster.EnemyPack;
+
+[EnumByName(typeof(Spr))]
+internal static partial class StableSpr { }
+
+[EnumByName(typeof(UK))]
+internal static partial class StableUK { }
